bf0036a17d8c36f6bd73e3b366c172265c01ff81
MSK-Asif <m.s.k.asif.041098@gmail.com>
2023-10-22 17:27:22 +0600

    service select page done

--- client/src/app/pages/booking/booking.component.ts
+++ client/src/app/pages/booking/booking.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.css']
+  styleUrls: ['./booking.component.css'],
 })
 export class BookingComponent {
-
+  date: string = '';
+  time: string = '';
 }
