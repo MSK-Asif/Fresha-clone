49a376b4147919964253a8ffce1f9460fdf75742
MSK-Asif <m.s.k.asif.041098@gmail.com>
2023-11-05 22:23:41 +0600

    feat: carousel and badges added

--- client/src/app/components/recommended/recommended.component.ts
+++ client/src/app/components/recommended/recommended.component.ts
@@ -7,17 +7,31 @@ import { ShopDataService } from 'src/app/services/shop-data.service';
   styleUrls: ['./recommended.component.css'],
 })
 export class RecommendedComponent {
-  public data: any[] | undefined;
-  public currentIndex = 0;
+  data!: object[];
+  currentIndex = 0;
 
   constructor(private dataService: ShopDataService) {}
 
   ngOnInit() {
     this.dataService.getAllShopData().subscribe((response) => {
       this.data = response; // All shop data
-      // console.log(this.data);
+      console.log(this.data);
     });
   }
 
-  
+  goToPrevious(): void {
+    this.currentIndex--;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.data.length - 1;
+    }
+    
+  }
+
+  goToNext(): void {
+    this.currentIndex++;
+    if (this.currentIndex >= this.data.length) {
+      this.currentIndex = 0;
+    }
+  }
+
 }
